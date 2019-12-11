8063675f4fb4f7e5b734b50561590272bd903cbf
Rakaneth <rakaneth@gmail.com>
2019-12-10 22:48:21 -0500

    add clang-format, basic data parse

--- src/gamedata.c
+++ src/gamedata.c
@@ -7,9 +7,10 @@ static ItemTemplate I_TEMPLATES[100];
 int readData(const char* fileName, DataType dType) {
     FILE* datafile = fopen(fileName, "r");
     int result;
-    
-    if (datafile == NULL) return 0;
-    switch(dType) {
+
+    if (datafile == NULL)
+        return 0;
+    switch (dType) {
         case DATA_CREATURE:
             result = getCreatureData(datafile);
             break;
@@ -25,15 +26,14 @@ int readData(const char* fileName, DataType dType) {
 int getCreatureData(FILE* f) {
     CreatureTemplate cur;
     int i = 0;
-    char buf[50];
-    char stat[5];
-    int value;
-    char glyph;
-    char name[20];
-    int statCheck;
-    int charCheck;
-    int nameCheck;
-    
+    char buf[50] = {0};
+    char stat[10] = {0};
+    int value = 0;
+    char glyph = 0;
+    char name[20] = {0};
+    int statCheck = 0;
+    int nameCheck = 0;
+
     cur.str = 10;
     cur.stam = 10;
     cur.spd = 10;
@@ -43,22 +43,26 @@ int getCreatureData(FILE* f) {
     strcpy(cur.name, "No name");
     cur.glyph = '\0';
 
-    while(fgets(buf, 50, f) != NULL) {
-        statCheck = sscanf(buf, "%[^: ]s%*[: ]%d", stat, &value);
-        charCheck = sscanf(buf, "%[^: ]s%*[: ]%c", stat, &glyph);
-        nameCheck = sscanf(buf, "%[^: ]s%*[: ]%s", stat, name);
+    while (fgets(buf, 50, f) != NULL) {
+        statCheck = sscanf(buf, "%[^: ]: %d", stat, &value);
+        nameCheck = sscanf(buf, "{%[^}:]: %c}", name, &glyph);
 
-        if (nameCheck == 2 && strcmp(stat, "NAME") == 0) {
+        if (nameCheck) {
             strcpy(cur.name, name);
-        } else if (charCheck == 2 && strcmp(stat, "GLYPH") == 0) {
             cur.glyph = glyph;
         } else if (statCheck == 2) {
-            if (strcmp(stat, "STR") == 0) cur.str = value;
-            if (strcmp(stat, "STAM") == 0) cur.stam = value;
-            if (strcmp(stat, "SPD") == 0) cur.spd = value;
-            if (strcmp(stat, "SKL") == 0) cur.skl = value;
-            if (strcmp(stat, "SAG") == 0) cur.sag = value;
-            if (strcmp(stat, "SMT") == 0) cur.smt = value;
+            if (strcmp(stat, "STR") == 0)
+                cur.str = value;
+            if (strcmp(stat, "STAM") == 0)
+                cur.stam = value;
+            if (strcmp(stat, "SPD") == 0)
+                cur.spd = value;
+            if (strcmp(stat, "SKL") == 0)
+                cur.skl = value;
+            if (strcmp(stat, "SAG") == 0)
+                cur.sag = value;
+            if (strcmp(stat, "SMT") == 0)
+                cur.smt = value;
         } else {
             C_TEMPLATES[i] = cur;
             cur.str = 10;
@@ -69,14 +73,13 @@ int getCreatureData(FILE* f) {
             cur.smt = 10;
             strcpy(cur.name, "No name");
             cur.glyph = '\0';
+            i++;
         }
-
-        i++;
     }
 
+    C_TEMPLATES[i] = cur;
+
     return 1;
 }
 
-int getItemData(FILE* f) {
-    return 1;
-}
+int getItemData(FILE* f) { return 1; }
